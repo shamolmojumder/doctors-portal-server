bca2d0ec181509528a2cca8a3d684ab92449a560
shamolmojumder <shamolmojumder14@gmail.com>
2021-12-19 02:22:53 +0600

    52-6 Create Server to store appointment in a database

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const  MongoClient  = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4xoys.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
+const app = express()
+
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const appointmentCollection = client.db("doctorsportal").collection("appointments");
+
+  app.post('/addAppointment',(req,res)=>{
+      const appointment=req.body;
+      appointmentCollection.insertOne(appointment)
+      .then(result=>{
+          res.send(result.insertedCount)
+      })
+  })
+});
+
+app.listen(process.env.PORT||port)
